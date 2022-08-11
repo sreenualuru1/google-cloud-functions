03a97da524249e10a580488cfda67bc6db7f0866
unknown <sreenualuru93@gmail.com>
2022-08-11 16:45:17 +0530

    Test Env Variables

--- gcs-to-pubsub-publisher/main.py
+++ gcs-to-pubsub-publisher/main.py
@@ -1,10 +1,23 @@
+import os
+
+from google.cloud import storage
+from google.cloud import service_account
+
+project_id = os.environ.get['project_id']
+topic_id = os.environ.get['pubsub_topic_id']
+print(f'project_id:{project_id}, topic_id:{topic_id}')
+
 def main(event, context):
     """Triggered by a change to a Cloud Storage bucket.
     Args:
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    file = event
-    print(f"Processing file: {file['name']}.")
-    print(event)
-    print(context)
+    gcs_path_prefix = 'test-sales-data'
+    print(event['contentType'], event['name'])
+    if event['contentType'] is 'text/csv' and event['name'].find(gcs_path_prefix):
+        print(f"Processing file: {event['name']}.")
+
+
+
+
