833665398179c55b70866fbd96d2d9d06d9f5e67
unknown <sreenualuru93@gmail.com>
2022-08-15 12:19:10 +0530

    Read csv file

--- gcs-to-pubsub-publisher/main.py
+++ gcs-to-pubsub-publisher/main.py
@@ -8,7 +8,7 @@ project_id = os.environ.get('project_id')
 topic_id = os.environ.get('pubsub_topic_id')
 bucket_name = os.environ.get('gcs_bucket_name')
 path_prefix = os.environ.get('gcs_path_prefix')
-logging.info('#### project:{project}, topic:{topic}, bucket:{gcs_bucket}, prefix:{prefix}'.format(
+print('#### project:{project}, topic:{topic}, bucket:{gcs_bucket}, prefix:{prefix}'.format(
     project=project_id, topic=topic_id, gcs_bucket=bucket_name, prefix=path_prefix))
 
 
@@ -18,12 +18,22 @@ def main(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    logging.info('#### filetype:{file_type}, filename:{file}'.format(
+    print('#### filetype:{file_type}, filename:{file}'.format(
         file_type=event['contentType'], file=event['name']))
 
     if event['contentType'] == 'text/csv' and event['name'].find(path_prefix):
         gcs_file_path = 'gs://'+bucket_name+'/'+event['name']
-        logging.info('#### Processing file: {file}'.format(file=gcs_file_path))
+        print('#### Processing file: {file}'.format(file=gcs_file_path))
         df = pd.read_csv(filepath_or_buffer=gcs_file_path,
                          storage_options={'token': 'cloud'})
-        logging.info(df.head(n=10))
+        print(df.head(n=10))
+
+
+# def hello_gcs(event, context):
+#     """Triggered by a change to a Cloud Storage bucket.
+#     Args:
+#          event (dict): Event payload.
+#          context (google.cloud.functions.Context): Metadata for the event.
+#     """
+#     file = event
+#     print(f"Processing file: {file['name']}.")
